d2a44fb2836c2ba7c03971524fc73cc1549a1875
ebodabebo <hartfield.ebony@gmail.com>
2021-12-12 23:28:38 -0600

    javascript coding updates

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const nineTask = document.getElementById('nine');
+const tenTask = document.getElementById('ten');
+const elevenTask = document.getElementById('eleven');
+const twelveTask = document.getElementById('twelve');
+const oneTask = document.getElementById('one');
+const twoTask = document.getElementById('two');
+const threeTask = document.getElementById('three');
+const fourTask = document.getElementById('four');
+const fiveTask = document.getElementById('five');
+const container = document.querySelector('.container');
+const timeBlockContainer = document.querySelector('.timeblock_container');
+
 let selectedTime, active 
 
-
+//Event Listeners
+timeBlockContainer.addEventListener('click', selectTask);
 
 // Select Time
 
@@ -16,6 +29,57 @@ function selectTime(time, color){
     }
 }
 
-function resetTasks(){
-    const allTimes = document.querySelectorAll('.ta')
+//Time Click
+function selectTime(e){
+    resetTasks();
+
+    taskColor = e.target.style.backgroundColor;
+
+    switch(e.target.id){
+        case 'nine':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'ten':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'eleven':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'twelve':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'one':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'two':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'three':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'four':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break
+        case 'five':
+            activeTask(nineTask, taskColor);
+            icon = '<i class="fas fa-clock></i>'
+            break        
+    }
+
+}
+
+function resetTime(){
+    const allTimes = document.querySelectorAll('.time')
+
+    allTimes.forEach((item)=>{
+        item.className = 'task_name';
+    })
 }
